19688b69ab2f85b792b5a691bd714ce728420810
ramonnokia00 <ramonsoares491@gmail.com>
2025-04-22 11:31:23 -0300

    commitando 1/2

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,15 @@
- /** @type {import('tailwindcss').Config} */
- export default {
-    content: ["./src/**/*.{html,js}"],
-    theme: {
-      extend: { 
-        colors: {
-        amarelo:  '#E6C744',
+ 
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        amarelo: '#E6C744',
         cinza: '#C2C8DA',
-
-      }
-    },
-    },
-    plugins: [],
-  }
+      },
+      
+  },
+  plugins: [],
+}
+}
